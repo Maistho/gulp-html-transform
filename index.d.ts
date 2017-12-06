410edddb93d9e09c7a2b88a00fbdeb95fee0f60f
Gustav Bylund <maistho@gmail.com>
2017-12-06 20:46:41 +0100

    rebuild typings

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="cheerio" />
 /// <reference types="node" />
 import * as stream from 'stream';
-export declare type Transformer = ($: CheerioStatic) => Promise<void> | void;
+export declare type Transformer = ($: CheerioStatic) => Promise<void>;
 export declare function transform(...args: Transformer[]): stream.Transform;
 export default transform;
