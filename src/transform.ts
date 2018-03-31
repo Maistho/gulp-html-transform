33b9e533c33cf1a91fc8b59fe530a9647ebac3ff
Gustav Bylund <maistho@gmail.com>
2018-03-31 02:51:24 +0200

    feat: add TransformerExtras
    Contains dirname - the directory of the file being parsed

--- src/transform.ts
+++ src/transform.ts
@@ -3,7 +3,11 @@ import * as cheerio from 'cheerio'
 import * as File from 'vinyl'
 import * as stream from 'stream'
 
-export type Transformer = ($: CheerioStatic) => Promise<void>
+export interface TransformerExtras {
+  dirname: string,
+}
+
+export type Transformer = ($: CheerioStatic, extras: TransformerExtras) => Promise<void>
 
 export function transform (...args: Transformer[]): stream.Transform {
   return through.obj(async (file: File, enc, cb) => {
@@ -12,11 +16,14 @@ export function transform (...args: Transformer[]): stream.Transform {
     }
 
     const content = file.contents.toString()
+    const dirname = file.dirname
     const $ = cheerio.load(content)
 
     for (const transformer of args) {
       try {
-        await transformer($)
+        await transformer($, {
+          dirname,
+        })
       } catch (err) {
         return cb(err)
       }
